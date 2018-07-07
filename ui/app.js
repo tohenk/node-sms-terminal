e645e0f5876beeb2376b90fab4c2d0e8858bfa5a
Toha <tohenk@yahoo.com>
2018-07-07 23:23:01 +0700

    Fix session storage path.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- ui/app.js
+++ ui/app.js
@@ -14,13 +14,13 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // session
 app.use(session({
-    store: new FileStore,
+    store: new FileStore({path: path.join(__dirname, '..', 'sessions')}),
     secret: 'nt-sms-terminal',
     resave: true,
     saveUninitialized: true
