6d47787ef6055858a7e14676314a5c8b19ef064e
Toha <tohenk@yahoo.com>
2018-07-11 08:44:10 +0700

    Fix activity stream flags again.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- term.js
+++ term.js
@@ -80,9 +80,7 @@ AppTerm.init = function(config) {
 AppTerm.initializeLogger = function() {
     this.logdir = this.config.logdir || path.join(__dirname, 'logs');
     this.logfile = path.join(this.logdir, 'activity.log');
-    this.stdout = new fs.createWriteStream(this.logfile, {
-        flags: fs.existsSync(this.logfile) ? 'r+' : 'w'
-    });
+    this.stdout = new fs.createWriteStream(this.logfile, {flags: 'a'});
     this.logger = new console.Console(this.stdout);
 }
 
